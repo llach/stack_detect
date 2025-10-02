39734934c08dea385ccc5539c0f069701d07bbc9
joriol <joriol@iri.upc.edu>
2025-10-02 13:40:37 +0200

    add server to start

--- src/stack_approach/launch/se_setup.launch.py
+++ src/stack_approach/launch/se_setup.launch.py
@@ -1,5 +1,5 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
 from launch.substitutions import LaunchConfiguration, LaunchConfiguration
 
 from launch_ros.actions import Node
@@ -61,6 +61,37 @@ def generate_launch_description():
             executable="static_transform_publisher",
             arguments="--frame-id right_arm_l515_link --child-frame-id camera_link".split(" "),
             output="both",
+        )
+    )
+
+    nodes_to_start.append(
+        DeclareLaunchArgument('port', default_value='8383', description='Web server port')
+    )
+
+    nodes_to_start.append(
+        SetEnvironmentVariable(name='PORT', value=LaunchConfiguration('port'))
+    )
+
+    nodes_to_start.append(
+        Node(
+            package='softenable_display',
+            executable='server',
+            name='softenable_display_server',
+            output='screen'
+        )
+    )
+    nodes_to_start.append(
+        Node(
+            package='softenable_display',
+            executable='display_service',
+            name='softenable_display_service',
+            output='screen',
+            parameters=[
+                {'server_host': 'localhost'},
+                {'server_port': LaunchConfiguration('port')},   # reuse the same port
+                {'endpoint': '/update'},
+            ],
+            arguments=['--ros-args', '--log-level', 'rmw_cyclonedds_cpp:=error']  
         )
     )
     return LaunchDescription(nodes_to_start)  
